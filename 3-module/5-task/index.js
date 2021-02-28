116b81227fb54930141f4e3f02d4626e03d178ff
Roman Paidem <romanramzespaydem@gmail.com>
2021-02-28 14:13:51 +0200

    задания 3.3 и 3.5

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,6 @@
 function getMinMax(str) {
   // ваш код...
+  let arr = str.split(/[ ,]+/);
+  arr = arr.filter((item) => item !== "" && isFinite(item));
+  return { min: Math.min(...arr), max: Math.max(...arr) };
 }
